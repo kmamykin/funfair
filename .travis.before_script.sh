e56b669932071af9fe41dd1f83e3935f2dfebcc9
Kliment Mamykin <kmamyk@yahoo.com>
2013-05-04 02:07:47 -0400

    Fixing travis!

--- .travis.before_script.sh
+++ .travis.before_script.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# guest:guest has full access to /
-
-#rabbitmqctl add_vhost /
-#rabbitmqctl add_user guest guest
-#rabbitmqctl set_permissions -p / guest ".*" ".*" ".*"
-
 # Drop all exchanges and queues before tests
 rabbitmqctl stop_app
 rabbitmqctl reset
